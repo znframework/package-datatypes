e59d0ea6a540695e126bd971a89394df534b62d1
Ozan UYKUN <znframework@gmail.com>
2018-01-19 11:45:39 +0300

    Added Package

---
+++ Strings/Split.php
@@ -0,0 +1,62 @@
+<?php namespace ZN\DataTypes\Strings;
+/**
+ * ZN PHP Web Framework
+ * 
+ * "Simplicity is the ultimate sophistication." ~ Da Vinci
+ * 
+ * @package ZN
+ * @license MIT [http://opensource.org/licenses/MIT]
+ * @author  Ozan UYKUN [ozan@znframework.com]
+ */
+
+use ZN\Datatype;
+
+class Split
+{
+    /**
+     * Split Upper Case
+     * 
+     * @param string $string
+     * 
+     * @return array
+     */
+    public static function upperCase(String $string) : Array
+    {
+        return Datatype::splitUpperCase($string);
+    }
+
+    //--------------------------------------------------------------------------------------------------------
+    // Apportion
+    //--------------------------------------------------------------------------------------------------------
+    //
+    // @param string  $string
+    // @param numeric $length
+    // @param string  $end
+    //
+    //--------------------------------------------------------------------------------------------------------
+    public static function apportion(String $string, Int $length = 76, String $end = "\r\n") : String
+    {
+        $arrayChunk = array_chunk(preg_split("//u", $string, -1, PREG_SPLIT_NO_EMPTY), $length);
+
+        $string = "";
+
+        foreach( $arrayChunk as $chunk )
+        {
+            $string .= implode("", $chunk) . $end;
+        }
+
+        return $string;
+    }
+
+    /**
+     * Divide
+     * 
+     * @param string $str       = NULL
+     * @param string $separator = '|'
+     * @param string $index     = '0'
+     */
+    public static function divide(String $str = NULL, String $separator = '|', String $index = '0')
+    {
+        return Datatype::divide($str, $separator, $index);
+    }
+}
